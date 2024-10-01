dd20f8d93795d56cd6ebd22261f098fbf681d01d
MazenSalahb <mazensalah360@gmail.com>
2024-10-01 21:49:52 +0300

    v1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/product-list-with-cart",
+  base: "/product-list-with-cart/",
   plugins: [react()],
 })
